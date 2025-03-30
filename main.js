6bc64b77269bc5e01e6778b6c895bd4f63ead579
Lucas Daniel <kingfoxtroi@gmail.com>
2025-03-30 12:26:18 -0300

    feat: improves handling of deleted message author verification

--- main.js
+++ main.js
@@ -68,14 +68,15 @@ client.on('message', message => {
 });
 
 client.on('message_revoke_everyone', async (after, before) => {
-    const messageText = `@${before.author.split('@')[0]} enviou uma mensagem e apagou! \n Mensagem: ${before.body}`;
-    if (before) {
-        console.log(before);
+    if (before && before.author) {
+        const messageText = `@${before.author.split('@')[0]} enviou uma mensagem e apagou! \n Mensagem: ${before.body}`;
         return await client.sendMessage(before.from, messageText, {
             mentions: [before.author]
         });
+    } else if (before) {
+        const messageText = `Uma mensagem foi apagada! \n Mensagem: ${before.body}`;
+        return await client.sendMessage(before.from, messageText);
     }
-    return await client.sendMessage(before.from, messageText);
 });
 
 client.on('ready', () => {
